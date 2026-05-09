ff1b86fbb90c4188047c0b78a7de7004f3610e7e
xgnico <nash3dv2@proton.me>
2026-05-09 14:32:12 +0400

    Clean up unused variables in scoreboard.cpp
    Removed unused player and team information variables.

--- cl_dll/hud/scoreboard.cpp
+++ cl_dll/hud/scoreboard.cpp
@@ -31,14 +31,6 @@
 #include "vgui_parser.h"
 #include <ctype.h>
 
-hud_player_info_t   g_PlayerInfoList[MAX_PLAYERS+1]; // player info from the engine
-extra_player_info_t	g_PlayerExtraInfo[MAX_PLAYERS+1]; // additional player info sent directly to the client dll
-team_info_t         g_TeamInfo[MAX_TEAMS+1];
-hostage_info_t      g_HostageInfo[MAX_HOSTAGES+1];
-int g_iUser1;
-int g_iUser2;
-int g_iUser3;
-int g_iTeamNumber;
 
 
 // X positions
